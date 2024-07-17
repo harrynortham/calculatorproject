9dd9d6a0bc19f364bc2c8ab6924745e9e5f810a5
Harry Northam <hello@harrynortham.com>
2024-07-17 17:25:27 +0200

    Calculator elements and basic styling added

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("hello");
+let displayValue = "";
+let number = 0;
+let operator = "";
+let secondNumber = 0;
+let displayElement = document.getElementById("calculator__display");
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+console.log(displayElement);
+displayElement.value = "nothing to display";
+
+function operate(number, operator, secondNumber) {
+  switch (operator) {
+    case "add":
+      add(number, secondNumber);
+      break;
+    case "subtract":
+      subtract(number, secondNumber);
+      break;
+    case "multiply":
+      multiply(number, secondNumber);
+      break;
+    case "divide":
+      divide(number, secondNumber);
+      break;
+  }
+}
