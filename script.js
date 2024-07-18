976851515c9f56a1cc5b06814e9330420412efae
Harry Northam <hello@harrynortham.com>
2024-07-18 11:53:23 +0200

    added eventlistener for clicks on the buttons

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ let display = document.getElementById("calculator__display__text");
 let buttonContainer = document.getElementById("calculator__buttons");
 let buttons = buttonContainer.getElementsByTagName("button");
 
-console.log(buttons);
-
 function add(a, b) {
   return a + b;
 }
@@ -22,6 +20,10 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function updateDisplay(value) {
+  display.innerText = value;
 }
 
 function operate(number, operator, secondNumber) {
@@ -40,3 +42,13 @@ function operate(number, operator, secondNumber) {
       break;
   }
 }
+
+//make array from html collection and give each one click function
+const buttonActions = Array.from(buttons).forEach((element) => {
+  element.addEventListener("click", () => {
+    updateDisplay(element.id);
+    // add here a switch that will perform different function for each key
+
+    //same function for the number keys
+  });
+});
